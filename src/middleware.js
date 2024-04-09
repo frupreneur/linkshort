a4682b8d4b2d26826eed1f34dd8249e4bd336008
frupreneur <frupreneur@gmail.com>
2024-04-09 22:07:53 +0100

    canada travel guide link added

--- src/middleware.js
+++ src/middleware.js
@@ -25,7 +25,7 @@ export const onRequest = defineMiddleware((context, next) => {
 	}
 	if (source == "canada-travel-guide") {
 		return Response.redirect(
-			`https://usa-canada-travel.axfest.com/usa-canada-visa-requirements-jobs-loans-programs-immigrants`,
+			`https://usa-canada-travel.englishcompositions.com/canada-visitor-visa-requirements/`,
 			301
 		);
 	}
